4b414bbd62f9cb4f15885991b4605ca5bc620667
Gabriel Soares <120689729+GabrielnSoares@users.noreply.github.com>
2025-02-27 21:30:07 -0300

    filtrando resultados da base com querymethod

--- src/main/java/br/gabnsoares/dev/demojpa/repository/UserRepository.java
+++ src/main/java/br/gabnsoares/dev/demojpa/repository/UserRepository.java
@@ -1,10 +1,18 @@
 package br.gabnsoares.dev.demojpa.repository;
 
 import br.gabnsoares.dev.demojpa.entity.UserEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
+
+    Page<UserEntity> findByName(String name, PageRequest pageRequest);
+
+    Page<UserEntity> findByAgeGreaterThanEqual(Long age, PageRequest pageRequest);
+
+    Page<UserEntity> findByNameAndAgeGreaterThanEqual(String name, Long age, PageRequest pageRequest);
 
 }
